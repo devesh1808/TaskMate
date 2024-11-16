e74d37d6c3201fcd1d5585585592724924444d8a
devesh1808 <devesh1808@gmail.com>
2024-11-16 18:49:44 +0530

    created a state named tasklist inside App.jsx to handle the tasklist

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
+import { useState } from "react";
 import { Header } from "./components/Header";
 import { AddTask } from "./components/AddTask";
 import { ShowTask } from "./components/ShowTask";
 import "./App.css";
 
 function App() {
+  const [tasklist, setTasklist] = useState([]);
+
   return (
     <>
       <Header />
