66df1665519399c1f98a42868bf6f88a4fbcc56d
devesh1808 <devesh1808@gmail.com>
2024-11-16 14:17:23 +0530

    created the UI for header inside Header.jsx component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,21 @@
 import React from "react";
+import Logo from "../assets/logo.svg";
 
 export const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header>
+      <div className="logo">
+        <img src={Logo} alt="Taskmate Logo" />
+        <span>Taskmate</span>
+      </div>
+      <div className="themeSelector">
+        <span className="light"></span>
+        <span className="medium"></span>
+        <span className="dark activeTheme"></span>
+        <span className="gOne"></span>
+        <span className="gTwo"></span>
+        <span className="GThree"></span>
+      </div>
+    </header>
+  );
 };
