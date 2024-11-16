0870a22476b4ab1a94d3a57ae3e3a5f5a0e51fbb
devesh1808 <devesh1808@gmail.com>
2024-11-16 16:51:55 +0530

    created the UI for showing the tasks inside ShowTask.jsx component

--- src/components/ShowTask.jsx
+++ src/components/ShowTask.jsx
@@ -1,5 +1,25 @@
 import React from "react";
 
 export const ShowTask = () => {
-  return <div>ShowTask</div>;
+  return (
+    <section className="showTask">
+      <div className="head">
+        <div>
+          <span className="title">Todo</span>
+          <span className="count">0</span>
+        </div>
+        <button className="clearAll">Clear All</button>
+      </div>
+      <ul>
+        <li>
+          <p>
+            <span className="name">Task A</span>
+            <span className="time">02:09:01 AM 14/09/2030 </span>
+          </p>
+          <i className="bi bi-pencil-square"></i>
+          <i className="bi bi-trash"></i>
+        </li>
+      </ul>
+    </section>
+  )
 };
