4b5fba9867b337d670fe79e64dfa38a4fe388560
devesh1808 <devesh1808@gmail.com>
2024-11-16 19:21:01 +0530

    added handleSubmit func for form handling and added a new array newTask to setTaskLlist state inside AddTask.jsx component

--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -1,9 +1,21 @@
 import React from "react";
 
 export const AddTask = ({ tasklist, setTasklist }) => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const date = new Date();
+    const newTask = {
+      id: date.getTime(),
+      name: e.target.task.value,
+      time: `${date.toLocaleTimeString()} ${date.toLocaleDateString()}`,
+    };
+    setTasklist([...tasklist, newTask]);
+  };
+
   return (
     <section className="addTask">
-      <form>
+      <form onSubmit={handleSubmit}>
         <input
           type="text"
           name="task"
