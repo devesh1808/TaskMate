12f2836c482c18ae70d07e4f8a0631b59ceae0b3
devesh1808 <devesh1808@gmail.com>
2024-11-16 18:56:47 +0530

    removed the temp array tasks and added tasklist array from state inside ShowTask.jsx component

--- src/components/ShowTask.jsx
+++ src/components/ShowTask.jsx
@@ -1,12 +1,6 @@
 import React from "react";
 
 export const ShowTask = ({ tasklist, setTasklist }) => {
-  const tasks = [
-    { id: 1001, name: "Task A", time: "02:09:01 AM 14/09/2030" },
-    { id: 1002, name: "Task B", time: "02:09:01 AM 15/09/2030" },
-    { id: 1003, name: "Task C", time: "02:09:01 AM 16/09/2030" },
-  ];
-
   return (
     <section className="showTask">
       <div className="head">
@@ -17,7 +11,7 @@ export const ShowTask = ({ tasklist, setTasklist }) => {
         <button className="clearAll">Clear All</button>
       </div>
       <ul>
-        {tasks.map((task) => (
+        {tasklist.map((task) => (
           <li key={task.id}>
             <p>
               <span className="name">{task.name}</span>
