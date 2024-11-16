0d44882afbcf7216180bbc476d50ddf60488a6e2
devesh1808 <devesh1808@gmail.com>
2024-11-16 16:35:47 +0530

    created the UI for adding the task inside AddTask component

--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -1,5 +1,18 @@
 import React from "react";
 
 export const AddTask = () => {
-  return <div>AddTask</div>;
+  return (
+    <section className="addTask">
+      <form>
+        <input
+          type="text"
+          name="task"
+          autoComplete="off"
+          placeholder="add task"
+          maxLength="25"
+        />
+        <button type="submit">Add</button>
+      </form>
+    </section>
+  );
 };
